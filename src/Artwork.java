ae3efa3662bdbe8a127d7207fe8497243e18d094
leosrabello <122031549+leosrabello@users.noreply.github.com>
2024-05-22 11:45:36 -0300

    New Commit

---
+++ src/Artwork.java
@@ -0,0 +1,61 @@
+public class Artwork {
+    private String title;
+    private String artist;
+    private String location;
+    private String AwType;
+    private int releaseYear;
+
+    public Artwork(String title, String artist, String location, String AwType, int releaseYear) {
+        this.title = title;
+        this.artist = artist;
+        this.location = location;
+        this.AwType = AwType;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getArtist() {
+        return artist;
+    }
+    
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    
+    public String getLocation() {
+        return location;
+    }
+    
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    public String getAwType() {
+        return AwType;
+    }
+    
+    public void setAwType(String awType) {
+        AwType = awType;
+    }
+    
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    @Override
+    public String toString() {
+        return "\nTitle: " + title + "\nArtist: " + artist + "\nLocation: " + location +
+                "\nArtwork Type: " + AwType + "\nYear of release: " + releaseYear;
+    }
+}
---
+++ src/DataStructure.java
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class DataStructure {
+
+    public static void saveArtworks(List<Artwork> works, String archive) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archive, true))) {
+            for (Artwork work : works) {
+                bw.write(work.getTitle() + "," + work.getArtist() + "," + work.getLocation() + "," +
+                         work.getAwType() + "," + work.getReleaseYear());
+                bw.newLine();
+            }
+            System.out.println("Artworks saved successfully");
+        } catch (IOException e) {
+            System.err.println("Error saving artworks: " + e.getMessage());
+        }
+    }
+}
